dcfb79aa997503c264950c0afc5c2241c9a1563f
Michael Chiu <chiumichel@gmail.com>
2019-02-20 03:30:43 -0500

    stylistic changes

--- include/EmpiricalDistribution.hpp
+++ include/EmpiricalDistribution.hpp
@@ -21,10 +21,9 @@
     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
     DEALINGS IN THE SOFTWARE.
 */
+
 #include "Zip.hpp"
-//std libs
 #include <vector>
-// boost libs
 #include "boost/math/distributions.hpp"		// includes all distributions
 
 namespace ejd {
